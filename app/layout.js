51f97b6b870cb8de816a12bd60b04824f0079fd8
Nur-Alam-Limon <nuralam201425@gmail.com>
2025-05-20 16:02:00 +0600

    Done basic setup and navbar

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+
+const JetBrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrainsMono",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"]
+});
+
+export const metadata = {
+  title: "Portfolio",
+  description: "Portfolio Website of Nur Alam",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={JetBrainsMono.variable}
+      >
+        <Header/>
+        {children}
+      </body>
+    </html>
+  );
+}
